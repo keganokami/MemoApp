c7095cf2f32e7378be97f2ea89645091ade69f6c
miyata taisei <taisei@miyatanoMacBook-Air.local>
2022-01-21 14:51:25 +0900

    impl memoListScreen and detail

--- App.jsx
+++ App.jsx
@@ -1,23 +1,10 @@
-import { StyleSheet, View } from 'react-native';
-
-import AppBar from './src/components/AppBar';
-import CircleButton from './src/components/CircleButton';
-import MemoList from './src/components/MemoList';
+import React from 'react';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoListScreen from './src/screens/MemoListScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <AppBar />
-      <MemoList />
-      <CircleButton>+</CircleButton>
-    </View>
+    // <MemoListScreen />
+    <MemoDetailScreen />
   );
 }
-
-// 基本は縦方向
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F0F4F8',
-  },
-});
