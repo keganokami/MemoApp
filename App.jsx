37251581bbdcbda230d3db1e595859ce5f960f5c
miyata taisei <taisei@miyatanoMacBook-Air.local>
2022-01-21 16:47:23 +0900

    replace keybordavoidngview

--- App.jsx
+++ App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
-// import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoCreateScreen from './src/screens/MemoCreateScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 // import MemoListScreen from './src/screens/MemoListScreen';
 
 export default function App() {
   return (
     // <MemoListScreen />
-    // <MemoEditScreen />
-    <MemoCreateScreen />
+    <MemoEditScreen />
+    // <MemoCreateScreen />
     // <MemoDetailScreen />
   );
 }
